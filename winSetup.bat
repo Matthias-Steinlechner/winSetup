5fb2331eff7d091ef70192aeab15340e684261fd
Matthias Steinlechner <71282874+Matthias-Steinlechner@users.noreply.github.com>
2023-09-26 16:25:49 +0200

    Update winSetup.bat

--- winSetup.bat
+++ winSetup.bat
@@ -1,40 +1,20 @@
 @echo off
 
-:: Initialize variables for team selections and installation status
+:: Initialize variables for team selections
 set /p "isOps=Are you in the Ops Team? (y/n): "
 set /p "isDev=Are you in the Dev Team? (y/n): "
 set /p "isManagement=Are you in the Management Team? (y/n): "
-set "successfulInstalls="
-set "failedInstalls="
-
-:: Define a list of software IDs to install
-set "softwareList=Mozilla.Firefox Google.Chrome Notion.Notion Microsoft.Teams Microsoft.Office TheDocumentFoundation.LibreOffice KeePassXCTeam.KeePassXC IrfanSkiljan.IrfanView GIMP.GIMP"
-
-:: Function to install software and track installation status
-:installSoftware
-set "app=%1"
-shift
-set "retryCount=0"
-:retry
-winget install --id %app%
-if %errorlevel% equ 0 (
-    set "successfulInstalls=%successfulInstalls% %app%"
-) else (
-    set /a "retryCount+=1"
-    if %retryCount% lss 3 (
-        echo Failed to install %app%. Retrying (%retryCount%)...
-        timeout /t 5 /nobreak >nul
-        goto retry
-    ) else (
-        set "failedInstalls=%failedInstalls% %app% (Error: %errorlevel%)"
-    )
-)
-goto :eof
 
 :: Install Standard Software
-for %%s in (%softwareList%) do (
-    call :installSoftware "%%s"
-)
+winget install -e --id Mozilla.Firefox
+winget install -e --id Google.Chrome
+winget install -e --id Notion.Notion
+winget install -e --id Microsoft.Teams
+winget install -e --id Microsoft.Office
+winget install -e --id TheDocumentFoundation.LibreOffice
+winget install -e --id KeePassXCTeam.KeePassXC
+winget install -e --id IrfanSkiljan.IrfanView
+winget install -e --id GIMP.GIMP
 
 :: Install Software Based on Team Selections
 if /i "%isOps%"=="y" (
@@ -44,20 +24,16 @@ if /i "%isOps%"=="y" (
 
 if /i "%isDev%"=="y" (
     :: Dev Team: Install development-related software
-    call :installSoftware "Git.Git"
-    call :installSoftware "OpenJS.NodeJS"
-    call :installSoftware "Docker.DockerDesktop"
-    call :installSoftware "JetBrains.PHPStorm"
+    winget install -e --id Git.Git
+    winget install -e --id OpenJS.NodeJS
+    winget install -e --id Docker.DockerDesktop
+    winget install -e --id JetBrains.PHPStorm
 )
 
 if /i "%isManagement%"=="y" (
     :: Management Team: Install management-related software
-    call :installSoftware "AgileBits.1Password"
+    winget install -e --id AgileBits.1Password
 )
-
-:: Display installation results
-echo Successful installations:%successfulInstalls%
-echo Failed installations:%failedInstalls%
 
 :: Pause to keep the command window open
 pause
